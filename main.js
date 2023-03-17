36e0f47d0991b7cc8377ffa396a0e1bdb5321566
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-03-17 23:29:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require('querystring');
 
 function templateHTML(title,list,body){
   return `
@@ -13,6 +14,8 @@ function templateHTML(title,list,body){
 <body>
 <h1><a href="/">WEB</a></h1>
 ${list}
+<a href = '/create'>create</a> 
+<a href = '/update'>update</a> 
 ${body}
 </body>
 </html>
@@ -29,11 +32,11 @@ function templateList(filelist){
   list = list + '</ul>';
   return list;
 }
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-
   if (pathname == "/") {
     if (queryData.id === undefined) {
       fs.readdir("./data", function (error, filelist) {
@@ -60,7 +63,37 @@ var app = http.createServer(function (request, response) {
         });
     });
     }
-  } else {
+  }else if(pathname =='/create'){
+    fs.readdir("./data", function (error, filelist) {
+      var title = "Web - create";
+      var list = templateList(filelist);
+      var template = templateHTML(title,list,`
+        <form action = "http://localhost:3000/create_process" 
+        method="post">
+            <p><input type = "text" name = "title" placeholder="title"></p>
+            <p><textarea name = "description"placeholder="description"></textarea>/></p>
+            <p><input type = "submit"></p>
+        </form>
+        `);
+      response.writeHead(200);
+      response.end(template);
+    });
+  }else if(pathname == '/create_process'){
+    var body = '';
+    request.on('data',function(data){
+      body = body + data;
+    });
+    request.on('end',function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+
+      fs.writeFile(`data/${title}`,description,'utf8',function(err){
+      response.writeHead(302, {Location: `/?id=${title}`});
+      response.end();
+      })
+    });
+  }else {
     response.writeHead(404);
     response.end("Not found");
   }
