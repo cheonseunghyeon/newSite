8c1c62432bd9417ea119edd7f64d344fea97c522
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-03-28 23:10:20 +0900

    Module

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var http = require("http");
+var url = require("url");
 
-// http 모듈의 createServer 메소드를 호출하여 HTTP 서버 생성
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"}); // (1)
-  response.write("Hello World");  // (2)
-  response.end();                 // (3)
-}).listen(8888);
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    // handle은 pathname와 request handler의 관계를 담고 있는 객체
+    route(handle, pathname);
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
